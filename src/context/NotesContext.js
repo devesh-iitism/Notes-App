c0c015428b67fd1c9fa89d5a35cae5ee49d52ac5
unknown <dcdev001@gmail.com>
2021-06-10 05:26:27 +0530

    delete a particular note on tapping the delete icon

--- src/context/NotesContext.js
+++ src/context/NotesContext.js
@@ -3,8 +3,11 @@ import createDataContext from './createDataContext';
 const notesReducer = (state, action) => {
     switch(action.type) {
         case 'add_notes':
-            return [...state, {title: `Notes #${state.length+1}`}];
+            return [...state, {id: Math.floor(Math.random()*99999), title: `Notes #${state.length+1}`}];
 
+        case 'delete_notes':
+            return state.filter((notes) => notes.id !== action.payload);
+        
         default:
             return state;
     }
@@ -16,8 +19,14 @@ const addNotes = (dispatch) => {
     };
 };
 
+const deleteNotes = (dispatch) => {
+    return (id) => {
+        dispatch({type: 'delete_notes', payload: id});
+    };
+};  
+
 export const { Context, Provider } = createDataContext(
     notesReducer, 
-    {addNotes},
+    {addNotes, deleteNotes},
     []
 );
--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context } from '../context/NotesContext';
 import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
-    const {state, addNotes} = useContext(Context);
+    const {state, addNotes, deleteNotes} = useContext(Context);
 
     return <View>
         <Button title="Add Notes" onPress={addNotes}/>
@@ -13,8 +13,10 @@ const IndexScreen = () => {
             keyExtractor={(note) => note.title}
             renderItem={({item}) => {
                 return <View style={styles.row}>
-                    <Text style={styles.title}>{item.title}</Text>
-                    <Feather style={styles.icon} name="trash"/>
+                    <Text style={styles.title}>{item.title} - {item.id}</Text>
+                    <TouchableOpacity onPress={() => deleteNotes(item.id)}>
+                        <Feather style={styles.icon} name="trash"/>
+                    </TouchableOpacity>
                 </View>
             }}
         />
